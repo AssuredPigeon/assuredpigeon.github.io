5bb7da3e25d23eb9fd095be777277c17b9cfb836
AssuredPigeon <danieltornero4@gmail.com>
2022-10-03 18:27:57 -0700

    p.1

---
+++ login-registro/conexion-be.php
@@ -0,0 +1,11 @@
+<?php
+
+$conexion = mysqli_connect("localhost","root","","login-registro-bd");
+/*
+if ($conexion) {
+	echo "se conecto exitosamente a la bd";
+}else{
+	echo "no se pudo conectar a la bd";
+}
+*/
+?>
